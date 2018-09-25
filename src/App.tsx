d6e3034b1c7248d23e81c1748e45cc52dd874257
Ted Driggs <ted.driggs@outlook.com>
2018-09-25 09:38:29 -0400

    Remove test data from initial state

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,12 @@ import { createStore } from 'redux';
 import './App.css';
 
 import { Playing } from './layouts/playing';
-import { Game, Gin, Player } from './models';
 import { reducer } from './Reducer';
 import { INITIAL_STATE } from './State';
 
-const games: Game[] = [
-    {
-        winner: Player.Two,
-        points: 10,
-        gin: Gin.Normal,
-    },
-    {
-        winner: Player.One,
-        points: 20,
-        gin: Gin.None,
-    },
-    {
-        winner: Player.Two,
-        points: 3,
-        gin: Gin.Super,
-    },
-    {
-        winner: Player.Two,
-        points: 90,
-        gin: Gin.None,
-    },
-];
-
 const store = createStore(
     reducer,
-    {
-        ...INITIAL_STATE,
-        player1Name: 'Jaclyn',
-        player2Name: 'Ted',
-        games,
-    },
+    INITIAL_STATE,
     // Hook enhancer for redux dev-tools
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
         (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
