60532224aa96c0258f7ac71fd1a5090ac0f8f865
Ted Driggs <ted.driggs@outlook.com>
2018-10-18 09:34:06 -0700

    Style updates to stats view

--- src/components/StatsViewer/StatsViewer.tsx
+++ src/components/StatsViewer/StatsViewer.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 import { nameOfPlayer, Player, PlayerNames } from 'src/models';
 import { PerPlayer, Stats } from 'src/models/stats';
+import './StatsViewer.css';
 
 export interface StatsViewerProps extends PlayerNames {
     className?: string;
@@ -15,21 +16,34 @@ export class StatsViewer extends React.Component<StatsViewerProps> {
         const players = { player1Name, player2Name };
         return (
             <div className={classNames('c-stats-viewer', props.className)}>
-                <h3>Wins</h3>
-                <ShareChart {...players} {...value.wins} />
-                <h3>Max Streak</h3>
-                <ShareChart {...players} {...value.maxStreak} />
-                <h3>Mean PPG</h3>
-                <ShareChart {...players} {...value.meanWinSize} />
-                <h3>Biggest Win</h3>
-                <ShareChart {...players} {...value.biggestWin} />
+                <ShareChart title="Wins" {...players} {...value.wins} />
+                <ShareChart
+                    title="Max Streak"
+                    {...players}
+                    {...value.maxStreak}
+                />
+                <ShareChart
+                    title="Mean PPG"
+                    {...players}
+                    {...value.meanWinSize}
+                />
+                <ShareChart
+                    title="Biggest Win"
+                    {...players}
+                    {...value.biggestWin}
+                />
             </div>
         );
     }
 }
 
-export const ShareChart: React.SFC<PlayerNames & PerPlayer> = props => (
+interface ShareChartProps extends PlayerNames, PerPlayer {
+    title: React.ReactNode;
+}
+
+export const ShareChart: React.SFC<ShareChartProps> = props => (
     <div className="c-share-chart">
+        <h3>{props.title}</h3>
         {nameOfPlayer(props, Player.One)}: {formatNumber(props[Player.One])}
         <br />
         {nameOfPlayer(props, Player.Two)}: {formatNumber(props[Player.Two])}
