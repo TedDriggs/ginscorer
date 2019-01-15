25f2185f303f20663266dffe167b21f906b1bf7a
Ted Driggs <ted.driggs@outlook.com>
2019-01-15 13:25:14 -0800

    Updates

---
+++ precache-manifest.714ea91a58ca2d6ae0bceb83dca0b194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbe57ffab963f0bb562",
+    "url": "/ginscorer/static/js/runtime~main.4dbe57ff.js"
+  },
+  {
+    "revision": "ef85183b3e5808bae0fb",
+    "url": "/ginscorer/static/js/main.ef85183b.chunk.js"
+  },
+  {
+    "revision": "5506a1c09c0f71386e29",
+    "url": "/ginscorer/static/js/1.5506a1c0.chunk.js"
+  },
+  {
+    "revision": "ef85183b3e5808bae0fb",
+    "url": "/ginscorer/static/css/main.400b9ddc.chunk.css"
+  },
+  {
+    "revision": "f9d796adb396e23c4f880dd53657db1c",
+    "url": "/ginscorer/index.html"
+  }
+];
