afd1c8a5c7c3ee064cb4bc1a96d5dae8882c8c35
Ted Driggs <ted.driggs@outlook.com>
2019-01-29 10:21:43 -0800

    Fix layout for iPhone X

--- src/components/ScrollViewer/index.tsx
+++ src/components/ScrollViewer/index.tsx
@@ -5,15 +5,15 @@ import './ScrollViewer.css';
 
 export interface ScrollViewerProps {
     className?: string;
-    overflowX?: 'hidden';
-    overflowY?: 'hidden';
+    overflowX?: 'hidden' | 'auto';
+    overflowY?: 'hidden' | 'auto';
 }
 
 export const ScrollViewer: React.SFC<ScrollViewerProps> = ({
     className,
     children,
-    overflowX = 'auto',
-    overflowY = 'auto',
+    overflowX = 'auto' as 'auto',
+    overflowY = 'auto' as 'auto',
 }) => (
     <div
         className={classNames('c-scroll-viewer', className)}
