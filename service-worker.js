89de32c9dc05f39391d31ff202462132ea0c30a0
Ted Driggs <ted.driggs@outlook.com>
2019-01-29 11:19:25 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/ginscorer/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/ginscorer/workbox-v3.6.3"});
 
 importScripts(
-  "/ginscorer/precache-manifest.14e0697b56f6693f84cf4a9eba818396.js"
+  "/ginscorer/precache-manifest.8b3ef1c21804044b6faf83f758b46636.js"
 );
 
 workbox.clientsClaim();
