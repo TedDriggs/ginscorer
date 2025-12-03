69cb52e389ea5bb80353217dc4026969b598e056
Ted Driggs <ted.driggs@outlook.com>
2025-12-03 08:03:57 -0800

    Remove custom service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { register } from './serviceWorkerRegistration';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
-register();
